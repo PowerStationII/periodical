7055c59d6a16565649d36a3ace269458964c3dca
jiangjj10001 <397177399@qq.com>
2015-10-28 22:22:03 +0800

    jiangjj

---
+++ src/main/java/com/cn/periodical/controller/editor/IssueController.java
@@ -0,0 +1,20 @@
+package com.cn.periodical.controller.editor;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+/**
+ * 签发人-签发管理Controller
+ * */
+@Controller
+public class IssueController extends EditorController{
+	
+	private static final Logger logger = LoggerFactory.getLogger(IssueController.class);
+
+}
