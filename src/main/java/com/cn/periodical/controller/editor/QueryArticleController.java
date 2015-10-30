d3657a3eeeadf5537b28c1ea9b1b94698cccd82d
jiangjj10001 <397177399@qq.com>
2015-10-30 20:21:07 +0800

    jiangjj

--- src/main/java/com/cn/periodical/controller/editor/QueryArticleController.java
+++ src/main/java/com/cn/periodical/controller/editor/QueryArticleController.java
@@ -13,14 +13,14 @@ import org.springframework.web.servlet.ModelAndView;
  * 稿件编辑-稿件查询Controller for editor
  * */
 @Controller
-public class ArticleQueryController extends EditorController{
+public class QueryArticleController extends EditorController{
 	
-	private static final Logger logger = LoggerFactory.getLogger(ArticleQueryController.class);
+	private static final Logger logger = LoggerFactory.getLogger(QueryArticleController.class);
 //	/**
 //	 * toNewArticlePage
 //	 * 新稿
 //	 */
-//	@RequestMapping(value="/toNewArticlePage",method = RequestMethod.GET)
+//	@RequestMapping(value="/toQueryArticle",method = RequestMethod.GET)
 //	public ModelAndView toNewArticlePage(@RequestParam("userId") String userId,
 //			HttpServletRequest request) {
 //		logger.info("审稿费Page:["+userId+"]");
