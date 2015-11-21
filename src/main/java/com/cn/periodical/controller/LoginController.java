1dd52f1b4752b77cdb2cad316eaea22a3660d7f0
broder1981 <broder1981@163.com>
2015-11-21 21:09:53 +0800

    songxw

--- src/main/java/com/cn/periodical/controller/LoginController.java
+++ src/main/java/com/cn/periodical/controller/LoginController.java
@@ -41,7 +41,7 @@ public class LoginController {
 	public ModelAndView login(@RequestParam("systemId") String systemId, @RequestParam("roleId") String roleId,
 			HttpServletRequest request) {
 		logger.info("登录系统systemId:[" + systemId + "]");
-		ModelAndView mav = new ModelAndView("portal");
+		ModelAndView mav = new ModelAndView("login");
 		mav.addObject("systemId", systemId);
 		mav.addObject("roleId", roleId);
 		return mav;
