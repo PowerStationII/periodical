5095832c0e5a872b9c65f676150975a592722f95
jiangjj10001 <397177399@qq.com>
2015-12-04 21:53:39 +0800

    jiangjj

--- src/main/java/com/cn/periodical/controller/editor/AdManageController.java
+++ src/main/java/com/cn/periodical/controller/editor/AdManageController.java
@@ -1,5 +1,10 @@
 package com.cn.periodical.controller.editor;
 
+import java.beans.PropertyEditorSupport;
+import java.math.BigDecimal;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
@@ -9,11 +14,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.alibaba.fastjson.JSON;
@@ -117,5 +123,99 @@ public class AdManageController extends EditorController{
 		ModelAndView mav = new ModelAndView("redirect:../editor/toAdManagerPage");
 
 		return mav;
+	}
+	
+	
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		binder.registerCustomEditor(Date.class, new DateEditor());
+		binder.registerCustomEditor(String.class, new StringEditor());
+		binder.registerCustomEditor(Integer.class, new IntegerEditor());
+		binder.registerCustomEditor(Long.class, new LongEditor());
+		binder.registerCustomEditor(Float.class, new FloatEditor());
+		binder.registerCustomEditor(Double.class, new DoubleEditor());
+		binder.registerCustomEditor(BigDecimal.class, new BigDecimalEditor());
+	}
+	
+	class DateEditor extends PropertyEditorSupport {
+		@Override
+		public void setAsText(String text) throws IllegalArgumentException {
+			if (text != null && !"".equals(text)) {
+				SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+				try {
+					setValue(dateFormat.parse(text));
+				} catch (ParseException e) {
+					setValue(null);
+				}
+			} else {
+				setValue(null);
+			}
+		}
+	}
+	
+	class StringEditor extends PropertyEditorSupport {
+		@Override
+		public void setAsText(String text) throws IllegalArgumentException {
+			if (text != null && !"".equals(text)) {
+				setValue(text);
+			} else {
+				setValue(null);
+			}
+		}
+	}
+	
+	class IntegerEditor extends PropertyEditorSupport {
+		@Override
+		public void setAsText(String text) throws IllegalArgumentException {
+			if (text != null && !"".equals(text)) {
+				setValue(Integer.valueOf(text));
+			} else {
+				setValue(null);
+			}
+		}
+	}
+	
+	class LongEditor extends PropertyEditorSupport {
+		@Override
+		public void setAsText(String text) throws IllegalArgumentException {
+			if (text != null && !"".equals(text)) {
+				setValue(Long.valueOf(text));
+			} else {
+				setValue(null);
+			}
+		}
+	}
+	
+	class FloatEditor extends PropertyEditorSupport {
+		@Override
+		public void setAsText(String text) throws IllegalArgumentException {
+			if (text != null && !"".equals(text)) {
+				setValue(Float.valueOf(text));
+			} else {
+				setValue(null);
+			}
+		}
+	}
+	
+	class DoubleEditor extends PropertyEditorSupport {
+		@Override
+		public void setAsText(String text) throws IllegalArgumentException {
+			if (text != null && !"".equals(text)) {
+				setValue(Double.valueOf(text));
+			} else {
+				setValue(null);
+			}
+		}
+	}
+	
+	class BigDecimalEditor extends PropertyEditorSupport {
+		@Override
+		public void setAsText(String text) throws IllegalArgumentException {
+			if (text != null && !"".equals(text)) {
+				setValue(BigDecimal.valueOf(Double.valueOf(text)));
+			} else {
+				setValue(null);
+			}
+		}
 	}
 }
